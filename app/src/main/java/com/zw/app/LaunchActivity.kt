348a5e0d776c591cdec717fd6974719c09421ed8
zhouwei <zhouwei10@zhouweideMacBook-Pro.local>
2023-05-06 10:07:56 +0800

    note

--- app/src/main/java/com/zw/app/LaunchActivity.kt
+++ app/src/main/java/com/zw/app/LaunchActivity.kt
@@ -1,10 +1,10 @@
 package com.zw.app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.zw.app.activity.Test1Activity
 
 class LaunchActivity : AppCompatActivity(), View.OnClickListener {
--- app/src/main/java/com/zw/app/activity/Test1Activity.java
+++ app/src/main/java/com/zw/app/activity/Test1Activity.java
@@ -3,6 +3,7 @@ package com.zw.app.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.zw.app.R;
 
@@ -12,5 +13,17 @@ public class Test1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1);
+    }
+
+    private void test1() {
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+        Log.i("TAG", "test1test1´");
+
     }
 }
